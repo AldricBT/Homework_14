c870542b5b1d518cf2f1b5a2ffdd315b67ce33b3
Александр Каменских <aldricBT@yandex.ru>
2023-09-04 16:45:44 +0500

    Немного добавлено. Начато изучение многооконных приложений

---
+++ ViewModels/AuthorizationWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Homework_12_notMVVM.Model.Workers;
+using Homework_12_notMVVM.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_12_notMVVM.ViewModels
+{
+    internal class AuthorizationWindowViewModel : ViewModel
+    {
+        #region Fields and properties
+
+        #region Worker
+        private Worker _worker;
+
+        /// <summary>
+        /// Выбранный работник. В виде текста textblock
+        /// </summary>
+        public Worker SelectedWorker
+        {
+            get => _worker;
+            set => Set(ref _worker, value);
+        }
+
+        #endregion
+        #endregion
+    }
+}
