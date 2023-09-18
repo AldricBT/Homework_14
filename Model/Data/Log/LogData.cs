58183c36a88eee3e597a472f185080d9c71ef7ff
Александр Каменских <aldricBT@yandex.ru>
2023-09-18 10:37:09 +0500

    добавление лога

---
+++ Model/Data/Log/LogData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_12_notMVVM.Model.Data.Log
+{
+    internal class LogData : DataBase<LogMessage>
+    {
+        
+        public LogData(string pathToData) 
+            : base(pathToData)
+        {
+          
+        }
+        /// <summary>
+        /// ID не используется в логах. Выбрасывает исключение
+        /// </summary>
+        /// <returns></returns>
+        public override int GetNewId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Model/Data/Log/LogMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_12_notMVVM.Model.Data.Log
+{
+    internal class LogMessage
+    {
+        private readonly DateTime _date;
+        private readonly string _message;
+
+        public DateTime Date { get => _date; }
+        public string Message { get => _message; }
+
+        public LogMessage(string message)
+        {
+            _date = DateTime.Now;
+            _message = message;
+        }
+
+       
+    }
+}
